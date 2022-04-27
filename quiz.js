e8caf307fa6bb2e5a28cfa02d57004c65f1a4b71
D.C. Lake <dclake@gmail.com>
2022-04-26 22:37:37 -0400

    Changed the level options from checkbox to radio

--- quiz.js
+++ quiz.js
@@ -29,135 +29,174 @@ var div_home = document.getElementById("home");
 
 // Question set
 let questions = [{
-    id: 1,
-    question: "Saint Lucia is a ________?",
-    answer: 2,
-    explanation: "Saint Lucia is a fertile island country in the Caribbean Sea. Its closest neighbours are the islands of Martinique, to the north, and Saint Vincent, to the south-west.",
-    readMore: "https://thecommonwealth.org/our-member-countries/saint-lucia#:~:text=Saint%20Lucia%20is%20a%20fertile,rivers%20and%20boiling%20sulphur%20springs.",
-    choices: [
-        "Planet",
-        "Island",
-        "Sports Team",
-        "Mythical Location"
-    ]
-},
-{
-    id: 2,
-    question: "What is the Capital City of Saint Lucia?",
-    answer: 3,
-    explanation: "Castries, capital and chief city of Saint Lucia island state, in the eastern Caribbean Sea.",
-    readMore: "https://www.britannica.com/place/Castries",
-    choices: [
-        "Bridgetown",
-        "Roseau",
-        "Castries",
-        "Canneries"
-    ]
-},
-{
-    id: 3,
-    question: "What Sea boarders the western coast of Saint Lucia?",
-    answer: 3,
-    explanation: "The beaches on the western side of the island front the tranquil turquoise waters of the Caribbean Sea and are favored for swimming and water sports.",
-    readMore: "https://www.tripsavvy.com/st-lucias-best-beaches-1488648",
-    choices: [
-        "Caspian Sea",
-        "Black Sea",
-        "Caribbean Sea",
-        "What Sea?"
-    ]
-},
-{
-    id: 4,
-    question: "Which is the official language of Saint Lucia?",
-    answer: 1,
-    explanation: "Saint Lucia’s official language is English, which is the island’s official language. During the 16th century, the British established a colony in the Caribbean, which gave rise to the language.",
-    readMore: "https://www.ilovelanguages.com/what-language-do-they-speak-in-st-lucia/",
-    choices: [
-        "English",
-        "French",
-        "German",
-        "Italian"
-    ]
-},
-{
-    id: 5,
-    question: " _____ serves as a second language of many Saint Lucians.",
-    answer: 3,
-    explanation: "Saint Lucian Creole French (Kwéyol), also known as Patois, is a French-speaking dialect of Saint Lucia. ",
-    readMore: "https://www.worldatlas.com/articles/what-languages-are-spoken-in-saint-lucia.html",
-    choices: [
-        "French",
-        "Papiamento",
-        "Patois/ Kweyol",
-        "Klingon"
-    ]
-},
-{
-    id: 6,
-    question: " This color is not present in the Saint Lucian Flag.",
-    answer: 2,
-    explanation: "The flag of Saint Lucia consists of a cerulean blue field charged with a yellow triangle in front of a white-edged black isosceles triangle.",
-    readMore: "https://en.wikipedia.org/wiki/Flag_of_Saint_Lucia",
-    choices: [
-        "White",
-        "Red",
-        "Yellow",
-        "Black"
-    ]
-},
-{
-    id: 7,
-    question: "What is the name of Saint Lucia's international Airport?",
-    answer: 3,
-    explanation: "There are two airports on the island of St. Lucia. Hewanorra International Airport at Vieux Fort (UVF) is located on the southern end of the island and serves primarily international flights. George F. L. Charles Airport (SLU), formerly known as 'Vigie Airport', is located on the outskirts of Castries in the north and is used for inter-island flights.",
-    readMore: "https://en.wikipedia.org/wiki/Hewanorra_International_Airport",
-    choices: [
-        "Argyle International Airport",
-        "Grantley Adams International Airport",
-        "Hewanorra International Airport",
-        "Piarco International Airport"
-    ]
-},
-{
-    id: 8,
-    question: "Saint Lucia is often referred to using the name of this famous woman from history:",
-    answer: 4,
-    explanation: " The tug-of-war between the Colonial powers of Enlgand & France, inspired one British historian to give Saint Lucia the nickname “Helen of the West Indies” comparing her to Helen of Troy",
-    readMore: "https://eastwinds.com/simply-beautiful-saint-lucia-helen-of-the-west-indies/",
-    choices: [
-        "Queen Victoria",
-        "Mary Magdalene",
-        "Joan of Arch",
-        "Helen of Troy"
-    ]
-},
-{
-    id: 9,
-    question: "What is the locally brewed beer of Saint Lucia called?",
-    answer: 1,
-    explanation: "Piton is a Pilsner beer brand from the island of Saint Lucia, brewed by Windward & Leeward Brewing Limited, which is owned by Heineken.",
-    readMore: "https://www.islandervillas.com/piton-beer-st-lucia.html",
-    choices: [
-        "Piton",
-        "Carib",
-        "Corona",
-        "Parbo"
-    ]
-},
-{
-    id: 10,
-    question: "Which Caribbean Island is first due south of St Lucia?",
-    answer: 2,
-    explanation: "Saint Lucia it is located north/northeast of the islands of Saint Vincent and the Grenadines,",
-    readMore: "https://www.travelpulse.com/destinations/caribbean/st-lucia.html",
-    choices: [
-        "Antigua",
-        "Saint Vincent",
-        "Barbados",
-        "Jamaica"
-    ]
-},
+        id: 1,
+        question: "Saint Lucia is a ________?",
+        answer: 2,
+        explanation: "Saint Lucia is a fertile island country in the Caribbean Sea. Its closest neighbours are the islands of Martinique, to the north, and Saint Vincent, to the south-west.",
+        readMore: "https://thecommonwealth.org/our-member-countries/saint-lucia#:~:text=Saint%20Lucia%20is%20a%20fertile,rivers%20and%20boiling%20sulphur%20springs.",
+        choices: [
+            "Planet",
+            "Island",
+            "Sports Team",
+            "Mythical Location"
+        ]
+    },
+    {
+        id: 2,
+        question: "What is the Capital City of Saint Lucia?",
+        answer: 3,
+        explanation: "Castries, capital and chief city of Saint Lucia island state, in the eastern Caribbean Sea.",
+        readMore: "https://www.britannica.com/place/Castries",
+        choices: [
+            "Bridgetown",
+            "Roseau",
+            "Castries",
+            "Canneries"
+        ]
+    },
+    {
+        id: 3,
+        question: "What Sea boarders the western coast of Saint Lucia?",
+        answer: 3,
+        explanation: "The beaches on the western side of the island front the tranquil turquoise waters of the Caribbean Sea and are favored for swimming and water sports.",
+        readMore: "https://www.tripsavvy.com/st-lucias-best-beaches-1488648",
+        choices: [
+            "Caspian Sea",
+            "Black Sea",
+            "Caribbean Sea",
+            "What Sea?"
+        ]
+    },
+    {
+        id: 4,
+        question: "Which is the official language of Saint Lucia?",
+        answer: 1,
+        explanation: "Saint Lucia’s official language is English, which is the island’s official language. During the 16th century, the British established a colony in the Caribbean, which gave rise to the language.",
+        readMore: "https://www.ilovelanguages.com/what-language-do-they-speak-in-st-lucia/",
+        choices: [
+            "English",
+            "French",
+            "German",
+            "Italian"
+        ]
+    },
+    {
+        id: 5,
+        question: " _____ serves as a second language of many Saint Lucians.",
+        answer: 3,
+        explanation: "Saint Lucian Creole French (Kwéyol), also known as Patois, is a French-speaking dialect of Saint Lucia. ",
+        readMore: "https://www.worldatlas.com/articles/what-languages-are-spoken-in-saint-lucia.html",
+        choices: [
+            "French",
+            "Papiamento",
+            "Patois/ Kweyol",
+            "Klingon"
+        ]
+    },
+    {
+        id: 6,
+        question: " This color is not present in the Saint Lucian Flag.",
+        answer: 2,
+        explanation: "The flag of Saint Lucia consists of a cerulean blue field charged with a yellow triangle in front of a white-edged black isosceles triangle.",
+        readMore: "https://en.wikipedia.org/wiki/Flag_of_Saint_Lucia",
+        choices: [
+            "White",
+            "Red",
+            "Yellow",
+            "Black"
+        ]
+    },
+    {
+        id: 7,
+        question: "What is the name of Saint Lucia's international Airport?",
+        answer: 3,
+        explanation: "There are two airports on the island of St. Lucia. Hewanorra International Airport at Vieux Fort (UVF) is located on the southern end of the island and serves primarily international flights. George F. L. Charles Airport (SLU), formerly known as 'Vigie Airport', is located on the outskirts of Castries in the north and is used for inter-island flights.",
+        readMore: "https://en.wikipedia.org/wiki/Hewanorra_International_Airport",
+        choices: [
+            "Argyle International Airport",
+            "Grantley Adams International Airport",
+            "Hewanorra International Airport",
+            "Piarco International Airport"
+        ]
+    },
+    {
+        id: 8,
+        question: "Saint Lucia is often referred to using the name of this famous woman from history:",
+        answer: 4,
+        explanation: " The tug-of-war between the Colonial powers of Enlgand & France, inspired one British historian to give Saint Lucia the nickname “Helen of the West Indies” comparing her to Helen of Troy",
+        readMore: "https://eastwinds.com/simply-beautiful-saint-lucia-helen-of-the-west-indies/",
+        choices: [
+            "Queen Victoria",
+            "Mary Magdalene",
+            "Joan of Arch",
+            "Helen of Troy"
+        ]
+    },
+    {
+        id: 9,
+        question: "What is the locally brewed beer of Saint Lucia called?",
+        answer: 1,
+        explanation: "Piton is a Pilsner beer brand from the island of Saint Lucia, brewed by Windward & Leeward Brewing Limited, which is owned by Heineken.",
+        readMore: "https://www.islandervillas.com/piton-beer-st-lucia.html",
+        choices: [
+            "Piton",
+            "Carib",
+            "Corona",
+            "Parbo"
+        ]
+    },
+    {
+        id: 10,
+        question: "Which Caribbean Island is first due south of St Lucia?",
+        answer: 2,
+        explanation: "Saint Lucia it is located north/northeast of the islands of Saint Vincent and the Grenadines.",
+        readMore: "https://www.travelpulse.com/destinations/caribbean/st-lucia.html",
+        choices: [
+            "Antigua",
+            "Saint Vincent",
+            "Barbados",
+            "Jamaica"
+        ]
+    },
+    {
+        id: 11,
+        question: "The iconic twin volcanic peaks in St Lucia are known as The Pitons, but what are their names?",
+        answer: 3,
+        explanation: "Gros Piton is located in Soufrière, and Petit Piton a little way off in Choiseul.",
+        readMore: "https://www.sandals.com/blog/pitons-st-lucia/#grospitonvspetitpitonwhatsthedifference",
+        choices: [
+            "Papa & Peewee Piton",
+            "Little & Large Piton",
+            "Gros & Petit Piton",
+            "Grande & Menor Piton"
+        ]
+    },
+    {
+        id: 12,
+        question: "Marigot Bay on the north coast of the island was famously the original setting for which 1967 film?",
+        answer: 2,
+        explanation: "The bay was used as the setting for the 1967 film adaptation of Hugh Lofting's Doctor Dolittle books. Scenes of the shipwreck, Great Pink Sea Snail, and the construction of the harness for the Giant Lunar Moth were filmed in the bay.",
+        readMore: "https://www.oasismarigot.com/history-of-marigot-bay-st-lucia/",
+        choices: [
+            "Jungle Book",
+            "Dr. Doolittle",
+            "Treasure Island",
+            "Bonnie and Clyde"
+        ]
+    },
+    {
+        id: 13,
+        question: "When did Saint Lucia become independent?",
+        answer: 1,
+        explanation: "On 22 February 1979, Saint Lucia became independent, as a constitutional monarchy and member of the Commonwealth.",
+        readMore: "https://www.britannica.com/place/Saint-Lucia/Independence",
+        choices: [
+            "22 February 1979",
+            "24 May 1976",
+            "29 September 1973",
+            "18 December 1962"
+        ]
+    },
 
 ]
 
@@ -167,12 +206,10 @@ function startQuiz() {
     let name = document.getElementById("name").value;
     if (name == "") {
         alert("You must answer a name first!")
-    }
-    else {
+    } else {
         if (question_count < 1) {
             alert("You must select a level!")
-        }
-        else {
+        } else {
             setCookie(name, 365)
             // if name = 
             // document.cookie = name;
@@ -182,12 +219,14 @@ function startQuiz() {
         }
     }
 }
+
 function setCookie(cname, exdays) {
     const d = new Date();
     d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
     let expires = "expires=" + d.toUTCString();
     document.cookie = cname + ";" + expires + ";path=/";
 }
+
 function setupQuiz() {
     hideResultButtons();
     hideNextButton();
@@ -266,6 +305,7 @@ function playQuiz() {
             alert("You must answer the question first!")
         }
     }
+
     function nullButtons() {
         A.onclick = null;
         B.onclick = null;
@@ -314,6 +354,7 @@ function playQuiz() {
     }
 
 }
+
 function setNo() {
     showNextButton();
     hideResultButtons();
@@ -323,14 +364,17 @@ function setNo() {
     playQuiz();
     clearExplanation();
 }
+
 function questionCounter() {
     count = question_number + "/" + question_count;
     div_question_number.innerHTML = count;
 }
+
 function showQuestions() {
     question = questions[current_question].question;
     div_question_text.innerHTML = question;
 }
+
 function showChoices() {
     let choice_text = "";
     let data_number = 1;
@@ -344,6 +388,7 @@ function showChoices() {
         data_number++;
     }
 }
+
 function checkAnswer() {
     correct_answer = questions[current_question].answer;
     let linkText = "Read More"
@@ -360,16 +405,19 @@ function checkAnswer() {
     }
     div_explanation.innerHTML = explanation;
 }
+
 function clearExplanation() {
     div_explanation.style.visibility = "hidden";
     div_explanation.style.display = "none";
 }
+
 function showResults() {
     div_header.innerHTML = "Results";
     clearBox();
     calculateScore();
     checkWin();
 }
+
 function resetQuiz() {
     question_number = 1;
     current_question = question_number;
@@ -380,16 +428,17 @@ function resetQuiz() {
     showNextButton();
     setNo();
 }
+
 function calculateScore() {
     score_percentage = (score / question_count) * 100;
 }
+
 function checkWin() {
     let name = document.cookie;
     if (score_percentage > passing_score) {
         result_text = "Congrats " + name + "!" + "<br>" + "You really know Saint Lucia!" +
             "<p>You got " + score + "/" + question_count + "<br>" + score_percentage + "%</p>";
-    }
-    else {
+    } else {
         result_text = "Sorry " + name + "!" + "<br>" + "You need to learn more about Saint Lucia!" +
             "<p>You got " + score + "/" + question_count + "<br>" + score_percentage + "%</p>";
     }
@@ -397,6 +446,7 @@ function checkWin() {
     hideNextButton();
     showResultButtons();
 }
+
 function clearBox() {
     div_choice_text.style.visibility = "hidden";
     div_choice_text.style.display = "none";
@@ -405,6 +455,7 @@ function clearBox() {
     // div_choice_text.innerHTML = "";
     clearExplanation();
 }
+
 function hideResultButtons() {
     // div_review.style.visibility = "hidden";
     // div_again.style.visibility = "hidden";
@@ -413,6 +464,7 @@ function hideResultButtons() {
     div_again.style.display = "none";
     div_home.style.display = "none";
 }
+
 function showResultButtons() {
     // div_review.style.visibility = "visible"
     // div_again.style.visibility = "visible";
@@ -421,16 +473,18 @@ function showResultButtons() {
     div_again.style.display = "inline";
     div_home.style.display = "inline";
 }
+
 function hideNextButton() {
     div_next.style.visibility = "hidden";
     div_next.style.display = "none"
 }
+
 function showNextButton() {
     div_next.style.visibility = "visible";
     div_next.style.display = "inline"
 }
+
 function hideStart() {
     div_setup.style.visibility = "hidden";
     div_setup.style.display = "none";
 }
-
